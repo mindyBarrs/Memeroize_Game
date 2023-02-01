d4249b5d329a11511db3c2f817cf883a02125f04
Mindy Barrs <mbarrs@deloitte.ca>
2023-02-01 18:54:46 -0500

    Initial Commit

---
+++ Memeroize/MemeroizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeroizeApp.swift
+//  Memeroize
+//
+//  Created by Barrs, Mindy on 2023-02-01.
+//
+
+import SwiftUI
+
+@main
+struct MemeroizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
